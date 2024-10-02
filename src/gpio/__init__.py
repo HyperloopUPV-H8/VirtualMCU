d0b73c4bbcdcc43259026c24fa1b29bf1e9d60ea
jmaralo <martinezalonsojn@gmail.com>
2024-10-02 15:53:23 +0200

    Create CTRL and GPIO shm region

--- src/gpio/__init__.py
+++ src/gpio/__init__.py
@@ -6,12 +6,12 @@ from .pinout import Pinout
 class GPIOMap:
     _pin_byte_size = 4 # CHANGE THIS VALUE TO MATCH THE FUTURE UNION TYPE
 
-    def __init__(self, name: str):
+    def __init__(self, name: str, create: bool = True):
         self._pins = len(Pinout)
 
-        self.mem = shared_memory.SharedMemory(
+        self._mem = shared_memory.SharedMemory(
             name=f"vmcu__gpio__{name}",
-            create=True,
+            create=create,
             size=self._pins * self._pin_byte_size
         )
 
@@ -19,31 +19,31 @@ class GPIOMap:
 
     def configure_pin(self, pin: Pinout, mode: PinMode):
         pin_offset = self._pin_offset(pin)
-        self.mem.buf[pin_offset] = mode.value
+        self._mem.buf[pin_offset] = mode.value
 
     # Passing a mode ensures the pin is in that mode, either by configuring the
     # pin or throwing an exception if the current mode is not the same
     def pin(self, pin: Pinout, mode: PinMode = None):
         pin_offset = self._pin_offset(pin)
-        current_mode = PinMode(self.mem.buf[pin_offset])
+        current_mode = PinMode(self._mem.buf[pin_offset])
         if mode is None:
             mode = current_mode
         elif current_mode is PinMode.NOT_USED:
-            self.mem.buf[pin_offset] = mode.value
+            self._mem.buf[pin_offset] = mode.value
         elif current_mode is not mode:
             raise InvalidModeError(pin, current_mode, mode)
 
-        pin_mem_slice = self.mem.buf[pin_offset:pin_offset + self._pin_byte_size]
+        pin_mem_slice = self._mem.buf[pin_offset:pin_offset + self._pin_byte_size]
         view = mode.get_view(pin, pin_mem_slice)
         self._pin_views[pin] = view
         return view
 
     def mem_size(self):
-        return len(self.mem.buf)
+        return len(self._mem.buf)
 
     def close(self):
-        self.mem.close()
-        self.mem.unlink()
+        self._mem.close()
+        self._mem.unlink()
 
     def _pin_offset(self, pin: Pinout):
         return pin.value * self._pin_byte_size
