49ff47bb3e9382a30e7d3b7db2587483cca5a77f
jmaralo <martinezalonsojn@gmail.com>
2024-10-31 22:28:17 +0100

    Add all missing memory access methods and fill in blank functions

--- src/pin/__init__.py
+++ src/pin/__init__.py
@@ -1,58 +1,95 @@
 from enum import Enum, auto, unique
+from shared_memory import SharedMemory
+
 from .pinout import Pinout
+from .memory import (
+    DigitalOut,
+    DigitalIn,
+    ADC,
+    EXTI,
+    Encoder,
+    InputCapture,
+    PWM,
+    DualPWM,
+)
 
 @unique
 class PinType(Enum):
     NotUsed = auto()
+    DigitalOut = auto()
+    DigitalIn = auto()
+    ADC = auto()
+    EXTI = auto()
+    Encoder = auto()
+    InputCapture = auto()
+    PWM = auto()
+    DualPWM = auto()
     # TODO: add missing types
 
-
-# TODO: fill in memory access classes
-class DigitalOut:
-    def __init__(self, shm: memoryview):
-        self._mem = shm
-
-    @property
-    def is_on(self) -> bool: 
-        return self._mem[0] != 0
-
-
-    @is_on.setter
-    def is_on(self, value: bool):
-        self._mem[0] = 1 if value else 0
-
-class DigitalIn: ...
-class ADC: ...
-class EXTI: ...
-class Encoder: ...
-class InputCapture: ...
-class PWM: ...
 class DualPWM: ...
 
 class Pin:
+    _pin_type_offset_in_memory = 0
+
+    _data: DigitalOut | DigitalIn | ADC | EXTI | Encoder | InputCapture | PWM | DualPWM
+
     def __init__(self, pin: Pinout, shm: memoryview, pin_type: PinType = None):
         self._pin = pin
-        self._get_memory_view(shm)
-        if (pin_type != None):
-            self._check_type(pin_type)
-        self._init_data()
+        self._mem = Pin._get_memory_view(pin, shm)
 
-    data: DigitalOut | DigitalIn | ADC | EXTI | Encoder | InputCapture | PWM | DualPWM
+        if (pin_type != None):
+            self._check_type_is_same_as(pin_type)
+        self._type = pin_type
+
+        self._init_data()
 
     @property
     def type(self) -> PinType:
-        # TODO: access shm
-        pass
+        return self._type
+    
+    @property
+    def data(self):
+        return self._data
 
-    def _get_memory_view(self, shm: memoryview):
-        # TODO: get a view of just the bytes of this pin
-        pass
+    # returns a memoryview of the exact bytes that represent this pin
+    def _get_memory_view(pin: Pinout, shm: memoryview) -> memoryview:
+        base_address = pin.value * SharedMemory.pin_size_in_memory
+        return shm[base_address:base_address + SharedMemory.pin_size_in_memory]
 
-    def _check_type(self, pin_type: PinType):
-        # TODO: check pin type in shm
-        pass
+    # throws an exception if the pin type is different
+    def _check_type_is_same_as(self, pin_type: PinType):
+        stored_pin_type = self._mem[Pin._pin_type_offset_in_memory]
+        if PinType(stored_pin_type) == pin_type:
+            return
+        raise DifferentPinType(pin_type, stored_pin_type)
 
+    # construct the class to access the actual pin data and store it
     def _init_data(self):
-        # TODO: init pin data with self.pin_type and self._memory
-        self.data = DigitalOut(self.mem)
-        pass
+        if self.type == PinType.DigitalOut:
+            self._data = DigitalOut(self._mem[1:])
+        elif self.type == PinType.DigitalIn:
+            self._data = DigitalIn(self._mem[1:])
+        elif self.type == PinType.ADC:
+            self._data = ADC(self._mem[1:])
+        elif self.type == PinType.EXTI:
+            self._data = EXTI(self._mem[1:])
+        elif self.type == PinType.Encoder:
+            self._data = Encoder(self._mem[1:])
+        elif self.type == PinType.InputCapture:
+            self._data = InputCapture(self._mem[1:])
+        elif self.type == PinType.PWM:
+            self._data = PWM(self._mem[1:])
+        elif self.type == PinType.DualPWM:
+            self._data = DualPWM(self._mem[1:])
+        # TODO: add missing types
+        else:
+            self._data = None
+
+
+class DifferentPinType(Exception):
+    def __init__(self, expected: PinType, got: PinType):
+        self.expected = expected
+        self.got = got
+
+    def __str__(self) -> str:
+        return f"pin declared as '{self.got}' when it should be '{self.expected}'"
