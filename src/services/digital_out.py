6f5d7d72487436ce2c8d3e2b3359bbc02af2de75
oganigl <osganig@gmail.com>
2024-11-14 16:21:22 +0100

    create encoder class

--- src/services/digital_out.py
+++ src/services/digital_out.py
@@ -2,7 +2,7 @@
 # EXAMPLE CODE
 class DigitalOutService:
     def __init__(self, pin: Pinout):
-        self._pin = SharedMemory.get_pin(pin);
+        self._pin = SharedMemory.get_pin(pin)
 
     def is_on(self):
         return self._pin.data.is_on
