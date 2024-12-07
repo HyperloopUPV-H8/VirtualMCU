59709ea767292f41cece77b48edd181456db2c8e
jdmarmen <jdmarmen@etsinf.upv.es>
2024-12-07 17:23:34 +0100

    Removed old test

--- Tests/Test_In-Out.py
+++
@@ -1,9 +0,0 @@
-from src.shared_memory import *
-from src.pin import *
-
-
-shm = SharedMemory("sim_tests")
-
-button = Pin(Pinout.PA0, shm._gpio_shm, PinType.DigitalIn)
-
-button._data.state = DigitalIn.State.High
