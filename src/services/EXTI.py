2580c33190a260233b966e42878e9948f4c12cfe
oganigl <osganig@gmail.com>
2024-12-16 23:04:50 +0100

    fix errors from exti

--- src/services/EXTI.py
+++ src/services/EXTI.py
@@ -1,17 +1,20 @@
 from src.shared_memory import SharedMemory
 from src.pin.pinout import Pinout
-import src.pin.memory as memory
+from src.pin import PinType,memory
 
 class EXTI:
-    def __init__(self, pin: Pinout, trigger_mode:memory.EXTI_Trigger_Mode):
-        self._pin= SharedMemory.get_pin(pin, memory.PinType.EXTI)
-        self._pin.trigger_signal = trigger_mode
+    def __init__(self,shm: SharedMemory,pin: Pinout, trigger_signal: bool):
+        self._pin= shm.get_pin(pin,PinType.EXTI)
+        self._pin.data.trigger_signal = trigger_signal
     
     def get_is_on(self) -> bool:
-        return self._pin.is_on
+        return self._pin.data.is_on
     
-    def set_priority(self, priority:int):
-        self._pin.priority = priority
+    def get_priority(self, priority:int):
+        self._pin.data.priority = priority
     
     def set_trigger_signal(self, trigger_signal:bool):
-        self._pin.trigger_signal = trigger_signal
+        self._pin.data.trigger_signal = trigger_signal
+    
+    def get_trigger_mode(self):
+        return  self._pin.data.trigger_mode
