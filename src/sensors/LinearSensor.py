cb8979ae7c9f85d4e2e4a6a2a1ff777deec903a5
jdmarmen <jdmarmen@etsinf.upv.es>
2024-12-31 17:29:55 +0100

    Addressed requested changes

--- src/sensors/LinearSensor.py
+++ src/sensors/LinearSensor.py
@@ -4,6 +4,7 @@ from src.shared_memory import SharedMemory
 from test_lib.input.aggregate import Multiple
 from test_lib.input.input import Input, InputFailedException
 from test_lib.condition.condition import Condition
+from test_lib.condition.checked import Checked
 
 class LinearSensor():
     def __init__(self,shm:SharedMemory,pin:Pinout,slope:float, offset:float):
@@ -15,13 +16,13 @@ class LinearSensor():
     class LinearSensorException(InputFailedException):
         def __str__(self):
             return "The ADC is not on"
-
-    def write(self,value:float) -> Input:
-        if(self._adc.get_is_on()):
-            self._value=value
-            adc_value=(self._value-self._offset)/self._slope
-            input_action=self._adc.generate_value(adc_value)
-            input_action.apply()
-            return self._adc.generate_value(adc_value)
-        else:
+    
+    def _check_pin_state(self) -> bool:
+        if not self._adc.get_is_on():
             raise self.LinearSensorException()
+        else:
+            return True
+
+    def generate_value(self, value: float) -> Input:
+        adc_value = (value - self._offset) / self._slope
+        return Checked(self._check_pin_state, self._adc.generate_value(adc_value))
