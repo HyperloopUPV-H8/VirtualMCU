a375718d428cb6a6adf8902451c4fc167cf34379
Jorge Sáez <jorgeesg82@gmail.com>
2025-01-04 12:28:29 +0100

    Modified comment

--- src/services/communication/packets/spipacket.py
+++ src/services/communication/packets/spipacket.py
@@ -2,22 +2,20 @@ import struct
 
 
 class SPIPacket:
-    """
-    Used to facilitates structured data transmission/reception through SPI.
+    """Class used to facilitates structured data transmission/reception through SPI.
 
-    args:
+    Args:
         types: format that defines data type in the packet.
-        See https://docs.python.org/es/3.13/library/struct.html#format-strings
+            See https://docs.python.org/es/3.13/library/struct.html#format-strings
     """
 
     def __init__(self, types: str):
         self.types = types
 
     def parse(self, data: bytes) -> tuple:
-        """
-        Transforms binary data into the distinct values based on defined types
+        """Transforms binary data into the distinct values based on defined types.
 
-        args:
+        Args:
             data: binary data received through SPI
         """
         if struct.calcsize(self.types) != len(data):
@@ -25,10 +23,9 @@ class SPIPacket:
         return struct.unpack(self.types, data)
 
     def build(self, *data) -> bytes:
-        """
-        Transforms the values given into binary data, ready to send through SPI
+        """Transforms the values given into binary data, ready to send through SPI
 
-        args:
+        Args:
             *data: all values you want to send, based on defined types
         """
         if struct.calcsize(self.types) != len(data):
