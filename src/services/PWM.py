c5062fa8a26524cd5bb989ec48a652629b762290
oganigl <osganig@gmail.com>
2025-01-06 20:03:44 +0100

    eliminate all the setters from PWM.py so people don't get confuse when using the class

--- src/services/PWM.py
+++ src/services/PWM.py
@@ -10,23 +10,11 @@ class PWM:
     def get_is_on(self) -> bool:
         return self._pin.is_on
     
-    def set_is_on(self, is_on:bool):
-        self._pin.is_on = is_on
-    
-    def set_frequency(self, frequency:int):
-        self._pin.frequency = frequency
-    
-    def set_duty_cycle(self, duty_cycle:float):
-        self._pin.duty_cycle = duty_cycle
-    
     def get_frequency(self) -> int:
         return self._pin.frequency
     
     def get_duty_cycle(self) -> float:
         return self._pin.duty_cycle 
-    
-    def set_dead_time_ns(self, dead_time_ns:int):
-        self._pin.dead_time_ns = dead_time_ns
     
     def get_dead_time_ns(self) -> int:
         return self._pin.dead_time_ns
